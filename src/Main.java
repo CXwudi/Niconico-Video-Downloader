37b9fded632a57af6f7e65363490bed6dc002fc2
CX无敌 <CX@192.168.56.1>
2017-10-20 22:45:22 -0400

    so now it can open a Chrome browser

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
 	WebDriver driver;
 	
+	public void openChrome() {
+		System.setProperty("webdriver.chrome.driver", "D:\\Program Files\\chromedriver.exe");
+		driver = new ChromeDriver();
+		
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-
+		Main myMainChrome = new Main();
+		myMainChrome.openChrome();
 	}
 
 }
