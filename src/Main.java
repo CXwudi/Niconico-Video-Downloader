7cf4ce377c8c51841e91c3d0c3939cd392933980
CX无敌 <CX@192.168.56.1>
2017-10-20 22:19:31 -0400

    try

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+
+public class Main {
+	WebDriver driver;
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("sdsd");
+
+	}
+
+}
