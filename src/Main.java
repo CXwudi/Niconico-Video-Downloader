6caff723d4b000e2fe6f0d7fd043252dfa15e558
CX无敌 <CX@192.168.56.1>
2017-10-23 19:52:11 -0400

    now, it can login into niconico website. :)

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,46 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
 	WebDriver driver;
+	String email = "1113421658@qq.com";
+	String password = "2010017980502";
+	//learn it by heart, there are 8 elements that can be searched by findElement() method.
+	//id, name, classname, tagname, cssSelector,
+	//linkText, partialLinkText, xpath
 	
 	public void openChrome() {
-		System.setProperty("webdriver.chrome.driver", "D:\\Program Files\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","D:\\Program Files\\chromedriver.exe");
 		driver = new ChromeDriver();
-		
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
 	}
 	
-	public static void main(String[] args) {
+	public void loginAndSetupNiconico() throws Exception {
+		driver.get("https://account.nicovideo.jp/login");
+		driver.findElement(By.id("input__mailtel")).sendKeys(email);
+		WebElement ps = driver.findElement(By.id("input__password"));
+		ps.sendKeys(password);
+		ps.submit();
+		
+		if (driver.getCurrentUrl().equals("http://www.nicovideo.jp/")) {
+			driver.findElement(By.id("areaTrigger")).click();
+			driver.findElement(By.className("selectType JP")).click();
+		} else
+			throw new Exception("nicovideo main website does not open!");
+	}
+	
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		Main myMainChrome = new Main();
-		myMainChrome.openChrome();
+		Main aMain = new Main();
+		aMain.openChrome();
+		aMain.loginAndSetupNiconico();
 	}
 
 }
