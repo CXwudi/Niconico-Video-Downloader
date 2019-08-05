09e7cc8ee35a3ccb3790adecda6be62695f8039b
CXwudi <1113421658@qq.com>
2019-08-05 13:22:09 -0400

    finish the unit test of ffmpeg extract audio

--- src/test/java/com/cxwudi/niconico_video_downloader/side_project/niconico_video_extractor/FFmpegProcessTest.java
+++ src/test/java/com/cxwudi/niconico_video_downloader/side_project/niconico_video_extractor/FFmpegProcessTest.java
@@ -12,39 +12,73 @@ import java.lang.ProcessBuilder.Redirect;
 
 import org.junit.Test;
 
+import ws.schild.jave.AudioAttributes;
 import ws.schild.jave.DefaultFFMPEGLocator;
+import ws.schild.jave.Encoder;
+import ws.schild.jave.EncoderException;
+import ws.schild.jave.EncoderProgressListener;
+import ws.schild.jave.EncodingAttributes;
+import ws.schild.jave.InputFormatException;
+import ws.schild.jave.MultimediaInfo;
+import ws.schild.jave.MultimediaObject;
 
 public class FFmpegProcessTest {
 
 	@Test
-	public void testProcess() throws IOException {
+	public void testExtractAudioWithProcessBuilder() throws IOException, InterruptedException {
 		DefaultFFMPEGLocator locator= new  DefaultFFMPEGLocator();
-        String exePath= locator.getFFMPEGExecutablePath();
-        System.out.println("ffmpeg executable found in <"+exePath+">");
-        File testFile = new File("いじめられっ子のルアン／初音ミク【雨の介】.mp4");
-		var ffpmegProcess = new ProcessBuilder(exePath,
-				"-i", testFile.getName(), //input file
+        var ffmpegPath = locator.getFFMPEGExecutablePath();
+        System.out.println("ffmpeg executable found in <"+ffmpegPath+">");
+        String testFile = "いじめられっ子のルアン／初音ミク【雨の介】.mp4";
+		var ffpmegProcessBuilder = new ProcessBuilder(ffmpegPath,
+				"-i", testFile, //input file
 				"-y", //answer yes for overwriting files
 				"-vn","-acodec","copy", //copy audio stream from video
-				testFile.getName().replace(".mp4", ".aac")); //output file
-		ffpmegProcess.directory(new File("C:\\Users\\11134\\Videos\\ffmpeg-4.1.4-win64-static\\bin"));
-		ffpmegProcess = ffpmegProcess.redirectInput(Redirect.INHERIT).redirectError(Redirect.INHERIT);
-		ffpmegProcess.start();
-
+				testFile.replace(".mp4", ".aac")); //output file
+		ffpmegProcessBuilder.directory(new File("test_files"));
+		ffpmegProcessBuilder = ffpmegProcessBuilder.redirectOutput(Redirect.INHERIT).redirectError(Redirect.INHERIT).redirectErrorStream(true);
+		var ffpmegProcess = ffpmegProcessBuilder.start();
+		ffpmegProcess.waitFor();
         
-		
+		assertTrue(true);
 	}
-	private static void syncStream(InputStream input, StringBuilder sb, PrintStream out) {
-		try(var output = new BufferedReader(new InputStreamReader(input))){
-			String s;
-			
-			while ((s = output.readLine()) != null) {
-				if (sb != null) sb.append(s).append("\n");
-				out.println(s);
-			}
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	
+	@Test
+	public void testExtractAudioWithApis() throws IllegalArgumentException, InputFormatException, EncoderException {
+		var audio = new AudioAttributes();
+		audio.setCodec(AudioAttributes.DIRECT_STREAM_COPY);
+		
+		var attributes = new EncodingAttributes();
+		attributes.setAudioAttributes(audio);
+		attributes.setVideoAttributes(null);
+		
+		var ffmpegEncoder = new Encoder();
+		var directory = new File("test_files");
+		var input = new File(directory, "【初音ミクDark】休憩【keeno】.flv");
+		var output = new File(directory, "【初音ミクDark】休憩【keeno】.aac");
+		ffmpegEncoder.encode(new MultimediaObject(input), output, attributes,
+				new EncoderProgressListener() {
+					
+					@Override
+					public void sourceInfo(MultimediaInfo info) {
+						System.out.println("format: " + info.toString());
+						
+					}
+					
+					@Override
+					public void progress(int permil) {
+						System.out.println("current progress: " + (permil/10.0) + "%");
+						
+					}
+					
+					@Override
+					public void message(String message) {
+						System.err.println("message: ");
+						System.err.println(message);
+						
+					}
+				});
+		
+		assertTrue(output.exists());
 	}
 }
