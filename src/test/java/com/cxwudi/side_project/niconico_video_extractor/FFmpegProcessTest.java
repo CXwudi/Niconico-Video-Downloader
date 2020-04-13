61d7d75eb9dfbd79ed68f7c028fe579ee218d017
CXwudi <1113421658@qq.com>
2020-04-12 21:39:46 -0700

    add get ffmpeg version in test case

--- src/test/java/com/cxwudi/side_project/niconico_video_extractor/FFmpegProcessTest.java
+++ src/test/java/com/cxwudi/side_project/niconico_video_extractor/FFmpegProcessTest.java
@@ -1,32 +1,19 @@
 package com.cxwudi.side_project.niconico_video_extractor;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import ws.schild.jave.*;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
 import java.lang.ProcessBuilder.Redirect;
 
-import org.junit.jupiter.api.Test;
-
-import ws.schild.jave.AudioAttributes;
-import ws.schild.jave.DefaultFFMPEGLocator;
-import ws.schild.jave.Encoder;
-import ws.schild.jave.EncoderException;
-import ws.schild.jave.EncoderProgressListener;
-import ws.schild.jave.EncodingAttributes;
-import ws.schild.jave.InputFormatException;
-import ws.schild.jave.MultimediaInfo;
-import ws.schild.jave.MultimediaObject;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FFmpegProcessTest {
 
 	@Test
 	public void testExtractAudioWithProcessBuilder() throws IOException, InterruptedException {
-		DefaultFFMPEGLocator locator= new  DefaultFFMPEGLocator();
+		DefaultFFMPEGLocator locator= new DefaultFFMPEGLocator();
         var ffmpegPath = locator.getFFMPEGExecutablePath();
         System.out.println("ffmpeg executable found in <"+ffmpegPath+">");
         String testFile = "いじめられっ子のルアン／初音ミク【雨の介】.mp4";
@@ -80,5 +67,14 @@ public class FFmpegProcessTest {
 				});
 		
 		assertTrue(output.exists());
+	}
+
+	@Test
+	public void testVersion() throws IOException, InterruptedException {
+		DefaultFFMPEGLocator locator= new DefaultFFMPEGLocator();
+		var ffmpegPath = locator.getFFMPEGExecutablePath();
+		var ffmpegProcessBuilder = new ProcessBuilder(ffmpegPath, "--version");
+		var p = ffmpegProcessBuilder.inheritIO().start();
+		p.waitFor();
 	}
 }
